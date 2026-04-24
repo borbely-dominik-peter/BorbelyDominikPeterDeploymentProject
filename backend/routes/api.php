eb1e43e203078e7a101bdb18f49dcd3f932ac191
borbely-dominik-peter <borbely.dominik.peter@students.jedlik.eu>
2026-04-24 08:53:58 +0200

    deployment XLIII

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,19 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('country/{country}', function ($country) {
     $result = DB::table("ships")->where("country", "=", $country)->get();
-    if ($country == "Jedlik") {
-        $result = {
-            "id": 999,
-            "name": "Jedlik",
-            "class": "Jedlik-class",
-            "type": "Jedlik Cruiser",
-            "launched": "1903",
-            "main_gun_caliber": "102",
-            "country": "Hungary",
-            "created_at": null,
-            "updated_at": null
-        }
-    }
     if (count($result) == 0) {
         return response()->json(["msg" => "nothing found"], 418);
     }
